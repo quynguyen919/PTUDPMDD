18d212b1e44c24dc6554916d6377c5b8d4d87280
huanhv123 <huan.hv1786@sinhvien.hoasen.edu.vn>
2022-09-21 14:07:54 +0700

    buid BookShop-BTL

---
+++ BaiTapLon/BookShop/views/DetailBook.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const DetailBook = () => {
+  return (
+    <View>
+      <Text>DetailBook</Text>
+    </View>
+  )
+}
+
+export default DetailBook
