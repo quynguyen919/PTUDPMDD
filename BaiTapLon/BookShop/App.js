3c1f72835b6a4a64412c3e55e863ce684432782f
quynguyen919 <quy.ndm9039@sinhvien.hoasen.edu.vn>
2022-09-25 19:39:09 +0700

    home UI

--- BaiTapLon/BookShop/App.js
+++ BaiTapLon/BookShop/App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Home from './views/Home';
+import Cart from './views/Cart';
+
+const Tab = createBottomTabNavigator();
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Cart" component={Cart} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -14,7 +25,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
--- BaiTapLon/BookShop/views/Home.js
+++ BaiTapLon/BookShop/views/Home.js
@@ -1,12 +1,105 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, Image, TouchableOpacity, StyleSheet } from 'react-native'
 import React from 'react'
+
+const item = [
+  {id : 1, name: "Tôi thấy hoa Vàng trên cỏ xanh", price: 190000, image: require('../assets/images/toi_thay_hoa_Vang_tren_co_xanh.png')},
+  {id : 2, name: "Cho tôi xin một Vé đi tuổi thơ", price: 190000, image: require('../assets/images/220px-Cho_tôi_xin_một_Vé_đi_tuổi_thơ.png')},
+]
+
+// function ListItemScreen(pops){
+//   return(
+    
+//     )
+// }
+
+const HeaderComponent = () => {
+  return(
+    <View style={[{ flex: 1, alignItems: "left"}]}>
+      <Text style={styles.title}>Sách</Text>
+    </View>
+  );
+};
+
+const ItemSeperator = () => {
+  return(
+    <View style={styles.seperator} />
+  )
+};
+
+const ItemBox = ({item}) => (
+  <View style={styles.product}>
+    <Image style={styles.img_prod} source={{uri: item.image}} />
+    <View style={styles.prod_details}>
+      <TouchableOpacity>
+        <Text style={styles.prod_name}>{item.name}</Text>
+      </TouchableOpacity>
+      <Text style={styles.prod_price}>{item.price}</Text>
+    </View>
+  </View>
+);
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.list}>
+      <FlatList 
+        ListHeaderComponent={HeaderComponent}
+        ItemSeperatorComponent={ItemSeperator}
+        keyExtractor={(item, index) => index}
+        data={item}
+        renderItem={ItemBox}   
+      />
+        
     </View>
   )
 }
 
 export default Home
+
+
+const styles = StyleSheet.create({
+  
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "black",
+    marginLeft: 10,
+    marginTop: 10,
+  },
+  seperator: {
+    height: 1,
+    width: "100%",
+    backgroundColor: "black",
+    // flexDirection: "column",
+    flex: "center",
+  },
+  product: {
+    flex: 1,
+    flexDirection: "column",
+    margin: 10,
+    border: "1px solid black",
+    borderRadius: 10,
+    width: 150,
+  },
+  img_prod: {
+    width: 100,
+    height: 200,
+    marginTop: 10,
+    marginLeft: 25,
+  },
+  prod_details: {
+    flex: 1,
+    flexDirection: "column",
+    marginLeft: 10,
+  },
+  prod_name: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "black",
+    width: 150,
+  },
+  prod_price: {
+    fontSize: 13,
+    color: "black",
+  },
+  
+});
